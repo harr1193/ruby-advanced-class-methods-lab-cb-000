a175cb9aa2b124ac92adc5ba4a4fff6ad1961a73
Kyle Harris <k_harris95@live.com>
2018-03-27 21:23:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,4 +32,8 @@ class Song
   def self.find_by_name(song_name)
     self.all.detect { |song| song.name == song_name }
   end
+
+  def find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
 end
