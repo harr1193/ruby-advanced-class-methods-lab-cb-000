a794c1e3ae0ad36f7256cd8651c356ddc0084006
Kyle Harris <k_harris95@live.com>
2018-03-27 21:27:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ class Song
     self.find_by_name(song_name) || self.create_by_name(song_name)
   end
 
-  def alphabetical
+  def self.alphabetical
     self.all.sort_by{ |a, b| a <=> b }
   end
 end
